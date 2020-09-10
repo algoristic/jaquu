26d069310d6df93f9852304dd81350b9c7e89126
leweke <male233@001NTB1066.ad.aconso.com>
2020-09-10 11:46:08 +0200

    🐛 Correct timer function

--- src/components/TaskTimer.js
+++ src/components/TaskTimer.js
@@ -7,18 +7,18 @@ class TaskTimer extends Component {
     constructor(props) {
         super(props);
         this.state = {time: '. . .'};
-    }
-    render() {
         const { task } = this.props;
         const { type } = task;
         const taskType = types[type];
-        setTimeout(() => {
+        setInterval(() => {
             taskType.timerFunction(task, (res) => {
                 this.setState({
                     time: res
                 });
             });
         }, 1000);
+    }
+    render() {
         return (
             <div className='task-timer'>
                 {this.state.time}
