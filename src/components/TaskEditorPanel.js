fd931e97a00eda871ad0b71b65c6395d83adf98c
leweke <male233@001NTB1066.ad.aconso.com>
2020-09-16 14:37:36 +0200

    🐛 Also fade out when saving...

--- src/components/TaskEditorPanel.js
+++ src/components/TaskEditorPanel.js
@@ -9,22 +9,33 @@ class TaskEditorPanel extends Component {
         super(props);
         this.state = {}
 
+        this.fade = this.fade.bind(this);
+        this.save = this.save.bind(this);
         this.cancel = this.cancel.bind(this);
     }
 
-    cancel() {
+    fade(fn) {
         this.setState({fade: true});
         setTimeout(() => {
             this.setState({fade: null});
-            this.props.action();
+            fn();
         }, 500);
+    }
+
+    save() {
+        const { save, task } = this.props;
+        this.fade(() => save(task));
+    }
+
+    cancel() {
+        this.fade(this.props.action);
     }
 
     render() {
         const { task, save, editProperty } = this.props;
         return (
             <div className={'task-editor-panel' + (this.state.fade ? ' fade-out' : '')}>
-                <EditTaskDialogue task={task} save={save}
+                <EditTaskDialogue task={task} save={this.save}
                     editProperty={editProperty}>
                 </EditTaskDialogue>
                 <ClickableOverlay action={this.cancel} />
