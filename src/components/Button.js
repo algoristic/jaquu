c1be2b93cdfa5577205c169407e6af8b2ac857cf
leweke <male233@001NTB1066.ad.aconso.com>
2020-09-17 09:57:49 +0200

    💄 Impl proper color theme

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,15 +3,21 @@ import React from 'react'
 import Icon from './Icon'
 import './Button.css'
 
-export default function({ id, onClick, disabled, text, icon, title }) {
-    return (
-        <button id={id} className='button' onClick={onClick} disabled={disabled} title={title}>
+export default function({ id, onClick, disabled, text, icon, title, severity, condition }) {
+    let render = true;
+    if(typeof condition === 'boolean') {
+        render = condition
+    } else if (typeof condition === 'function') {
+        render = condition();
+    }
+    return render && (
+        <button id={id} className={'btn ' + (severity ? ('btn-' + severity) : 'btn-info')} onClick={onClick} disabled={disabled} title={title}>
             {icon && (
-                <span className='button-icon'>
+                <span className='btn-icon'>
                     <Icon name={icon} />
                 </span>
             )}
-            <span className='button-text'>{ text }</span>
+            <span className='btn-text'>{ text }</span>
         </button>
     );
 };
