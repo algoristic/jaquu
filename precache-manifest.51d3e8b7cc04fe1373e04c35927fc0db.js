c3ed9040f0b7cc8192e624b2bbfeedeadb8164df
leweke <male233@001NTB1066.ad.aconso.com>
2020-09-28 15:06:59 +0200

    Updates

---
+++ precache-manifest.51d3e8b7cc04fe1373e04c35927fc0db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00743689bea590dfd70857f1af221b34",
+    "url": "/jaquu/index.html"
+  },
+  {
+    "revision": "65655585e2c34bd2cc3b",
+    "url": "/jaquu/static/css/2.f290e260.chunk.css"
+  },
+  {
+    "revision": "96b486cd2a54f950cede",
+    "url": "/jaquu/static/css/main.b9788ef4.chunk.css"
+  },
+  {
+    "revision": "65655585e2c34bd2cc3b",
+    "url": "/jaquu/static/js/2.a8149a1d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/jaquu/static/js/2.a8149a1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b486cd2a54f950cede",
+    "url": "/jaquu/static/js/main.f96fcca9.chunk.js"
+  },
+  {
+    "revision": "6cae08b19e62a6b9ca6a",
+    "url": "/jaquu/static/js/runtime-main.60c7d6a7.js"
+  }
+]);
